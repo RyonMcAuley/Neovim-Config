30eebfafd16762af45413f7e825d20b8b710f268
RyonMcAuley <rdmcauley@gmail.com>
2026-04-04 21:29:40 -0400

    fix conflicting binds

--- lua/plugins/linux/nvimtmuxnavigation.lua
+++ lua/plugins/linux/nvimtmuxnavigation.lua
@@ -7,10 +7,17 @@ return {
             disable_when_zoomed = true -- defaults to false
         }
 
-        vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
-        vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
-        vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
-        vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+        if os.getenv('TMUX') then
+            vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+            vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+            vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+            vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+        else
+            vim.keymap.set('n', '<C-h>', '<C-w>h')
+            vim.keymap.set('n', '<C-j>', '<C-w>j')
+            vim.keymap.set('n', '<C-k>', '<C-w>k')
+            vim.keymap.set('n', '<C-l>', '<C-w>l')
+        end
         -- vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
         -- vim.keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
     end
