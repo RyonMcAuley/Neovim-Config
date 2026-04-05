38549a9911bccecb474b352153ba2b1e1fbb8623
RyonMcAuley <rdmcauley@gmail.com>
2026-04-04 20:11:10 -0400

    go lsp

--- lua/lsp/go-lsp.lua
+++ lua/lsp/go-lsp.lua
@@ -1,21 +1,24 @@
 local lspconfig = require("lspconfig")
 
 lspconfig.gopls.setup({
-  on_attach = function(client, bufnr)
-    -- add any buffer-local keymaps or cmp settings here
-    -- for example:
-    require('cmp').setup.buffer({
-      sources = {
-        { name = 'nvim_lsp' },
-      }
-    })
-  end,
-  settings = {
-    gopls = {
-      usePlaceholders = true,
-      completeUnimported = true,
-      staticcheck = true,
+    on_attach = function(client, bufnr)
+        -- add any buffer-local keymaps or cmp settings here
+        client.server_capabilities.workspace = {
+            didChangeWatchedFiles = {
+                dynamicRegistration = true,
+            },
+        }
+        require('cmp').setup.buffer({
+            sources = {
+                { name = 'nvim_lsp' },
+            }
+        })
+    end,
+    settings = {
+        gopls = {
+            usePlaceholders = true,
+            completeUnimported = true,
+            staticcheck = true,
+        },
     },
-  },
 })
-
